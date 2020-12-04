7a1abd433310ca72f2a6b49f6e0c4741f0c99b6a
Michael A Gallo <michaelgallo@protonmail.com>
2020-12-04 15:42:15 -0500

    added modules

--- blocks/blocks.h
+++ blocks/blocks.h
@@ -3,10 +3,12 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	// {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
+	{"", "memory",					10,		4},
 	{"", "volume",					0,		10},
+	{"", "battery",					20,		3},
 	{"", "clock",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim[] = " ";
+static unsigned int delimLen = 1;
